db64fd1da4c536d3fd3a0f9a21a6d7482f13ddfc
dnilssongr47-hub <dnilssongr47@gmail.com>
2026-01-16 00:14:12 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
+import { TooltipProvider } from "./components/ui/tooltip";
+import NotFound from "./pages/not-found";
+import Home from "./pages/home";
 
 function Router() {
   return (
